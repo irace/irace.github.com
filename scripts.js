b84501b4eeb4d3304fe2d1969d80d94f7a1c183a
Bryan Irace <bryan.irace@gmail.com>
2011-12-29 17:15:34 -0500

    Use HTML5 SoundCloud widget, clean up page

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function resizeTwitterWidget() {
 }
 
 function twitterTimelineHeight() {
-    return $(window).height() - 325;
+    return $(window).height() - 310;
 }
 
 $(window).resize(resizeTwitterWidget);
