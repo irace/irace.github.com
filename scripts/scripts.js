40d72d14c9a48add8eefbd23ac8c73a2482439bc
Bryan Irace <bryan.irace@gmail.com>
2011-12-30 13:07:40 -0500

    Move Twitter profile scripts to separate file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,11 +18,44 @@ _gaq.push(['_setDomainName', 'bryanirace.com']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
 })();
+
+$(function() {
+    // Twitter profile widget
+    new TWTR.Widget({
+        id: 'twitter-widget',
+        version: 2,
+        type: 'profile',
+        rpp: 10,
+        interval: 6000,
+        width: 220,
+        height: twitterTimelineHeight(),
+        theme:{
+            shell: {
+                background: '#333333',
+                color: '#ffffff'
+            },
+            tweets:{
+                background: 'whiteSmoke',
+                color: '#404040',
+                links: '#00438A'
+            }
+        },
+        features:{
+            scrollbar: true,
+            loop: false,
+            live: true,
+            hashtags: true,
+            timestamp: true,
+            avatars: false,
+            behavior: 'all'
+        }
+    }).render().setUser('irace').start();
+});
