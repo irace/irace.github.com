fd339f69b713aaf0459a490bbb4a8f03c3a60049
Bryan Irace <bryan.irace@gmail.com>
2014-02-25 16:05:45 -0500

    Prevent calling method on undefined

--- scripts/lastfm.js
+++ scripts/lastfm.js
@@ -11,7 +11,7 @@ var last_fm_client = {
     $.get('http://ws.audioscrobbler.com/2.0/', params).done(function (data) {
       var track = data.recenttracks.track[0];
 
-      if (track['@attr'].nowplaying == 'true') {
+      if (track.hasOwnProperty('@attr') && track['@attr'].nowplaying == 'true') {
         callback(true, track.artist['#text'], track.name);
       }
       else {
